99b31796a922ca0d520b7d774993c1fa20bce367
ViktorTovstyi <viktor.tovstyi@gmail.com>
2026-09-22 17:54:27 +0300

    optimized import.

--- src/test/java/org/machanism/machai/ai/manager/UsageStatisticsTest.java
+++ src/test/java/org/machanism/machai/ai/manager/UsageStatisticsTest.java
@@ -1,8 +1,8 @@
 package org.machanism.machai.ai.manager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.reflect.Constructor;
